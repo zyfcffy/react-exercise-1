c076985b8a3d70cb6cd668d831ba9ba12a6c9ab8
FuFangyuan <fangyuan.fu@thoughtworks.com>
2020-09-22 20:39:12 +0800

    [fufangyuan] complete education li

--- src/components/Education/Education.js
+++ src/components/Education/Education.js
@@ -21,6 +21,47 @@ class Education extends Component {
               </p>
             </section>
           </li>
+          <li>
+            <p className="year">
+              <strong>2005</strong>
+            </p>
+            <section>
+              <p>
+                <strong>Secondary school specializing in artistic</strong>
+              </p>
+              <p>
+                Eos, explicabo, nam, tenetur et ab eius deserunt aspernatur
+                ipsum ducimus quibusdam quis voluptatibus.
+              </p>
+            </section>
+          </li>
+          <li>
+            <p className="year">
+              <strong>2009</strong>
+            </p>
+            <section>
+              <p>
+                <strong>First level graduation in Graphic Design</strong>
+              </p>
+              <p>
+                Aspernatur, mollitia, quos maxime eius suscipit sed beatae
+                ducimus quaerat quibusdam perferendis? Iusto, quibusdam
+                asperiores unde repellat.
+              </p>
+            </section>
+          </li>
+
+          <li>
+            <p className="year">
+              <strong>2012</strong>
+            </p>
+            <section>
+              <p>
+                <strong>Secondary level graduation in Graphic Design</strong>
+              </p>
+              <p>Ducimus, aliquam tempore autem itaque et accusantium!</p>
+            </section>
+          </li>
         </ul>
       </div>
     );
